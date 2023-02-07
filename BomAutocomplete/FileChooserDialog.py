59206172e7c2c38cebc541d4a05e8e0d5519fa71
Matthias Reis <20595601+mattarroz@users.noreply.github.com>
2023-02-07 19:58:58 +0100

    First working version of component chooser dialog

--- BomAutocomplete/FileChooserDialog.py
+++ BomAutocomplete/FileChooserDialog.py
@@ -3,42 +3,35 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
+def FileChooser():
+    dialog = Gtk.FileChooserDialog(
+        title="Please choose a file", action=Gtk.FileChooserAction.OPEN
+    )
+    dialog.add_buttons(
+        Gtk.STOCK_CANCEL,
+        Gtk.ResponseType.CANCEL,
+        Gtk.STOCK_OPEN,
+        Gtk.ResponseType.OK,
+    )
+
+    filter_py = Gtk.FileFilter()
+    filter_py.set_name("Kicad Netlist (xml file)")
+    filter_py.add_mime_type("application/xml")
+    dialog.add_filter(filter_py)
+
+    response = dialog.run()
+    if response == Gtk.ResponseType.OK:
+        result = dialog.get_filename()
+    elif response == Gtk.ResponseType.CANCEL:
+        result = ""
+    dialog.destroy()
+    return result
+
+
 class FileChooserWindow(Gtk.Window):
     def __init__(self):
         super().__init__(title="FileChooser Example")
 
-        box = Gtk.Box(spacing=6)
-        self.add(box)
-
-        button1 = Gtk.Button(label="Choose File")
-        button1.connect("clicked", self.on_file_clicked)
-        box.add(button1)
-
-        button2 = Gtk.Button(label="Choose Folder")
-        button2.connect("clicked", self.on_folder_clicked)
-        box.add(button2)
-
-    def on_file_clicked(self, widget):
-        dialog = Gtk.FileChooserDialog(
-            title="Please choose a file", parent=self, action=Gtk.FileChooserAction.OPEN
-        )
-        dialog.add_buttons(
-            Gtk.STOCK_CANCEL,
-            Gtk.ResponseType.CANCEL,
-            Gtk.STOCK_OPEN,
-            Gtk.ResponseType.OK,
-        )
-
-        self.add_filters(dialog)
-
-        response = dialog.run()
-        if response == Gtk.ResponseType.OK:
-            print("Open clicked")
-            print("File selected: " + dialog.get_filename())
-        elif response == Gtk.ResponseType.CANCEL:
-            print("Cancel clicked")
-
-        dialog.destroy()
 
     def add_filters(self, dialog):
         filter_text = Gtk.FileFilter()
--- BomAutocomplete/main.py
+++ BomAutocomplete/main.py
@@ -1,5 +1,12 @@
 import os, sys
 import kicad_netlist_reader
+import ComponentChooserDialog
+import FileChooserDialog
+import re
+
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
 
 
 sys.path.append(os.getcwd() + "/../nexar-first-supply-query/python/SupplyQueryDemo")
@@ -10,7 +17,7 @@ query PartSearch($query: String) {
   supSearch(
     q: $query
     inStockOnly: true
-    limit: 4
+    limit: 20
   ) {
     hits
     results {
@@ -44,37 +51,81 @@ query PartSearch($query: String) {
 }
 '''
 
+
+def getDescriptionFromComponent(c):
+    PARTNAME_MAP = {'c': "Capacitor", "r": "Resistor"}
+
+    description = PARTNAME_MAP[str.lower(c.getPartName())]
+    if c.getValue() != "":
+        description += " " + c.getValue()
+    if c.getFootprint() != "":
+        description += " " + re.search(r"([0-9]{4})", c.getFootprint().split(":")[-1]).group(0)
+
+    description = description.replace("_", " ").replace(":", " ")
+
+    words = description.split(" ")
+
+    #we reverse to remove the duplicates starting from the end
+    words.reverse()
+    for word in words:
+        while words.count(word) > 1:
+            words.remove(word)
+        if word == "":
+            words.remove(word)
+    words.reverse()
+
+    description = " ".join(words)
+
+    return description
+
 if __name__ == '__main__':
 
     clientId = os.environ['NEXAR_CLIENT_ID']
     clientSecret = os.environ['NEXAR_CLIENT_SECRET']
     nexar = NexarClient(clientId, clientSecret)
 
-    mpn = input('Search MPN: ')
-    if not mpn:
-        sys.exit()
-
-    variables = {
-        'query': mpn
-    }
-
-    kicad_netlist_reader.netlist(sys.argv[1])
+    fname = ""
+    if len(sys.argv) < 2:
+        fname = FileChooserDialog.FileChooser()
+        if len(fname) == 0:
+            print("No file selected")
+            exit(1)
+    else:
+        fname = sys.argv[1]
+    net = kicad_netlist_reader.netlist(fname)
     # Get all of the components in groups of matching parts + values
     # (see kicad_netlist_reader.py)
     grouped = net.groupComponents()
+    # Output all of the component information
+    for group in grouped:
+        # Add the reference of every component in the group and keep a reference
+        # to the component so that the other data can be filled in once per group
+        c = group[0]
 
-    results = nexar.get_query(QUERY_PARTS, variables)
+        if len(c.getField("Mouser Part Number")):
+            continue
 
-    if results:
-        print(str(results))
-        for it in results.get("supSearchMpn", {}).get("results", {}):
-            print(str(it))
-            # print(f'MPN: {it.get("part", {}).get("mpn")}')
-            # print(f'Description: {it.get("part", {}).get("shortDescription")}')
-            # print(f'Manufacturer: {it.get("part", {}).get("manufacturer", {}).get("name")}')
-            # print(f'Lifecycle Status: {getLifecycleStatus(it.get("part", {}).get("specs", {}))}')
+        variables = {
+            'query': getDescriptionFromComponent(c)
+        }
+        results = nexar.get_query(QUERY_PARTS, variables)
+
+        if results:
+            parts = []
+            for it in results.get("supSearch", {}).get("results", {}):
+                parts.append([it['part']['manufacturer']['name'], it['part']['mpn'],
+                               it['part']['medianPrice1000']['price'] if it['part']['medianPrice1000'] else None])
+                # ,
+                # it['part']['sellers']['company']['name'] if it['part']['sellers'] else None,
+                # it['part']['sellers']['company']['homepageUrl'] if it['part']['sellers'] else None,
+                # it['part']['sellers']['offers']['name'] if it['part']['sellers'] else None
+
+            win = ComponentChooserDialog.ComboBoxWindow(parts)
+            win.connect("destroy", Gtk.main_quit)
+            win.show_all()
+            Gtk.main()
+
+        else:
+            print('Sorry, no parts found')
             print()
-    else:
-        print('Sorry, no parts found')
-        print()
 
